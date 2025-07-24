43b222b0abcf092ba06194d8eb0498bfd12fbccb
kommathotigopi <33325805+kommathotigopi@users.noreply.github.com>
2025-07-24 15:07:50 +0530

    Add files via upload

---
+++ ProductDbContext.cs
@@ -0,0 +1,20 @@
+﻿using CRUD_CF.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_CF
+{
+    public class ProductDbContext : DbContext
+    {
+        public DbSet<Products> products { get; set; }
+
+        public ProductDbContext(DbContextOptions options):base (options)
+        {
+            
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=DESKTOP-9390DJ5\\SQLEXPRESS; Database=CRUDDb;integrated security=True;TrustServerCertificate=true;");
+        //}
+    }
+    }
+
